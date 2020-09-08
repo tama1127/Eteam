6a1420e2c30661b1a1c8021fe624bb39cf77de37
crab-121622 <64264212+crab-121622@users.noreply.github.com>
2020-09-08 20:17:18 +0900

    Update mypage.php
    tmp_nameの一時ファイルの概念がよくわからない。。。

--- mypage.php
+++ mypage.php
@@ -34,6 +34,25 @@ mypage
   
   echo $bookname.<br>.$review.<br>.$comment.<br>.$readdate;
   
+/*写真ファイルをサーバーにアップロードする。
+  if(strlen($_FILES["pic"]["name"])>0)
+  {
+      $filename = $_FILES["pic"]["name"]
+      if(! move_uploaded_file($_FILES["pic"]["tmp_name"],$filename) )
+      {
+          echo "アップロードに失敗しました。<br>";
+      }else{
+          echo "name=";
+          echo $filename;
+          echo "<br>";
+          echo "<IMG src='$filename'>";
+          echo "<br>";
+      }
+    }else{
+          echo "ファイルをアップロードしてください";
+    }
+  
+  */
   ?>
 </body>
 
