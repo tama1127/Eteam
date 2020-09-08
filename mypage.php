bb55723141e394499b47dda11fac6511ea0985be
crab-121622 <64264212+crab-121622@users.noreply.github.com>
2020-09-08 19:28:45 +0900

    Update mypage.php

--- mypage.php
+++ mypage.php
@@ -13,20 +13,28 @@ mypage
 
 <body>
   
-  <form action = "" method = "post">
+  <form action = "" method = "post" enctype = "multipart/form-date">
     
-    //写真の挿入
-    //<input type = ""    
+    /*画像の表示はされない*/
+    作品アイコン<input type = "file" name = "pic"><br>    
     作品名<input type = "text" name = "bookname" placeholder = "作品名"><br>
     評価<input type = "number" name = "review" min = "1" max = "5" ><br>
     コメント<input type = "text" name = "comment" placeholder = "コメント"><br>
-    読み終えた日<input type = "date">
+    読み終えた日<input type = "date" name = "readdate">
+    
+    <input type = "submit" name = "submit">
    
   </form>
-
+ <?php
+  
   $bookname = $_POST["bookname"];
   $review = $_POST["review"];
+  $comment = $_POST["comment"];
+  $readdate = $_POST["readdate"];
   
+  echo $bookname<br>$review<br>$comment<br>$readdate;
+  
+  ?>
 </body>
 
 </html>
